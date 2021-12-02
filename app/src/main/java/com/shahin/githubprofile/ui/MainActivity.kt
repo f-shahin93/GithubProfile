eaeb31f760e1ec282f52a353343d522ae3aefc68
F_Shahin <f.shahin.kh@gmail.com>
2021-12-02 14:31:38 +0330

    Change main theme.setup bottom navigation in MainActivity.

--- app/src/main/java/com/shahin/githubprofile/ui/MainActivity.kt
+++ app/src/main/java/com/shahin/githubprofile/ui/MainActivity.kt
@@ -2,15 +2,19 @@ package com.shahin.githubprofile.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.shahin.githubprofile.R
 import com.shahin.githubprofile.app.MainApplication
-import com.shahin.githubprofile.databinding.ActivityMainBinding
 import com.shahin.githubprofile.di.MainActivitySubComponent
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var mainActivitySubComponent: MainActivitySubComponent
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -18,9 +22,26 @@ class MainActivity : AppCompatActivity() {
             (applicationContext as MainApplication).applicationGraph.mainComponent().create()
 
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initNavController()
 
-        DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+    }
 
+    private fun initNavController() {
+        val navView: BottomNavigationView = findViewById(R.id.bottom_nav_view)
+        navController = findNavController(R.id.nav_host_fragment)
+
+        navController.graph = navController.navInflater.inflate(R.navigation.navigation_graph)
+
+        navView.setupWithNavController(navController)
+        navView.setOnItemSelectedListener {
+            if (navController.currentDestination?.id == it.itemId) {
+                return@setOnItemSelectedListener false
+            } else {
+                NavigationUI.onNavDestinationSelected(it, navController)
+                return@setOnItemSelectedListener true
+            }
+        }
     }
 
 }
